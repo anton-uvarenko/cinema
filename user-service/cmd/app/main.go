20f70e8b420f8c6fbf4f0e227cff50812d8734f1
Artem Uvarenko <artem.uvarenko@pensionera.se>
2023-05-15 20:27:59 +0300

    feat(users-comment): added add comment logic

--- user-service/cmd/app/main.go
+++ user-service/cmd/app/main.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"github.com/anton-uvarenko/cinema/user-service/internal/core/database"
+	"github.com/anton-uvarenko/cinema/user-service/internal/core/repo"
+	"github.com/anton-uvarenko/cinema/user-service/internal/services"
+	transportGRPC "github.com/anton-uvarenko/cinema/user-service/internal/transport/grpc"
+	"github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
 func main() {
-	//db = database.SetUpConnection()
+	db := database.SetUpConnection()
+	r := repo.NewRepo(db)
+	s := services.NewServices(r)
+
+	listen, err := net.Listen("tcp", ":5000")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	server := grpc.NewServer()
+	transportGRPC.SetUpServerControllers(server, s)
+	err = server.Serve(listen)
+	if err != nil {
+		logrus.Fatal(err)
+	}
 }
